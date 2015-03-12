515e319531b28f44bedd06e2f1b4b723f85c6a48
Mike Ma <mikemandarine@gmail.com>
2015-03-13 00:16:03 +0100

    imported config.py

---
+++ config.py
@@ -0,0 +1,8 @@
+#! /bin/python
+# global settings for email service
+
+mailgun_key = '' # mailgun api key
+mailgun_sandbox = '' # mailgun sandbox host name
+mandrill_key = '' # mandrill api key
+sender_name =  '' # default sender name
+sender_email = '' # default sender email
