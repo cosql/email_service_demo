b7d6a22a689d07378050f8970ab05237ff89d4ac
Mike Ma <mikemandarine@gmail.com>
2015-04-05 15:35:35 +0200

    update default api keys

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 #! /bin/python
 # global settings for email service
 
-mailgun_key = '' # mailgun api key
-mailgun_sandbox = '' # mailgun sandbox host name
-mandrill_key = '' # mandrill api key
-sender_name =  '' # default sender name
-sender_email = '' # default sender email
+mailgun_key = 'key-c57536663caee62c3a30f367562b19c7'
+mailgun_sandbox = 'sandboxc90288b9f29f4d4997fb800e74d89111.mailgun.org'
+mandrill_key = 'LNLWrN12hPA6Afju_h3gWQ'
+sender_name =  'Email Service'
+sender_email = 'cosql@github.com'
