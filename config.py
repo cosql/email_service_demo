8a99bb14fcda8ba951f27b2bbc64bdc3f17cb154
Mike Ma <mikemandarine@gmail.com>
2015-04-15 13:28:21 +0200

    remove default config parameters

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 #! /bin/python
 # global settings for email service
 
-mailgun_key = 'key-c57536663caee62c3a30f367562b19c7'
-mailgun_sandbox = 'sandboxc90288b9f29f4d4997fb800e74d89111.mailgun.org'
-mandrill_key = 'LNLWrN12hPA6Afju_h3gWQ'
-sender_name =  'Email Service'
-sender_email = 'cosql@github.com'
+mailgun_key = ''
+mailgun_sandbox = ''
+mandrill_key = ''
+sender_name =  ''
+sender_email = ''
